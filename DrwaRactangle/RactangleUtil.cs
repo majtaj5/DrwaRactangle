3f96ac868c80f39363f6b32bdbbd9035ca95a311
Piotr Krol <krolpiotr225@gmail.com>
2022-05-28 21:38:09 +0200

    Add project files.

---
+++ DrwaRactangle/RactangleUtil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace DrwaRactangle
+{
+    class RactangleUtil
+    {
+        public void DrowRactangle(double wigth, double height,Point3d insPt)
+        {
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
+            var ed = doc.Editor;
+
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    doc.LockDocument();
+                    BlockTable bt = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+
+
+
+                }
+                catch (System.Exception ex )
+                {
+
+                    ed.WriteMessage("Error encountered: " + ex.Message);
+                    trans.Abort();
+                }
+            }
+
+        }
+    }
+}
