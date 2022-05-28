694c315a36a56d4a92c0a6c27303ca70c76be7a9
Piotr Krol <krolpiotr225@gmail.com>
2022-05-28 22:13:19 +0200

    Drowing the ractangle

--- DrwaRactangle/RactangleUtil.cs
+++ DrwaRactangle/RactangleUtil.cs
@@ -25,8 +25,34 @@ namespace DrwaRactangle
                 {
                     doc.LockDocument();
                     BlockTable bt = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                    BlockTableRecord btr = trans.GetObject(bt[BlockTableRecord.ModelSpace],OpenMode.ForWrite) as BlockTableRecord;
 
+                    //Convertthe insertionPoint to point2d
+                    Point2d insPt2d = new Point2d(insPt.X,insPt.Y);
 
+                    //Derive the upperleft corner based on the Insention Ponit
+                    Point2d ulPt = new Point2d(insPt.X, insPt.Y + height);
+
+                    //Derive the upper right corner based on the upper left corner
+                    Point2d urPt = new Point2d(ulPt.X + wigth, ulPt.Y);
+
+                    //Derive the lower right corner based on the insertion point
+
+                    Point2d lrPt = new Point2d(insPt.X + wigth, insPt.Y);
+
+                    //Draw the LWPolyline using the newly derived verticles
+                    Polyline pl = new Polyline();
+                    pl.AddVertexAt(0, insPt2d, 0, 0, 0);
+                    pl.AddVertexAt(1, ulPt, 0, 0, 0);
+                    pl.AddVertexAt(2, urPt, 0, 0, 0);
+                    pl.AddVertexAt(3, lrPt, 0, 0, 0);
+                    pl.Closed = true;
+
+                    btr.AppendEntity(pl);
+                    trans.AddNewlyCreatedDBObject(pl, true);
+                    trans.Commit();
+
+                    ed.WriteMessage("Rantangle is drawn successfully!");
 
                 }
                 catch (System.Exception ex )
